4b27c0116f8d39ca08197fa252c191f7c04e28db
zhangyuan <cool.zhangyy@gmail.com>
2020-06-15 01:25:32 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.IO;
+
+namespace CreateDateGroupFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootFolderPath = GetFolderPath();
+            //var folderPath = "D:\\Users\\zhangyuan\\Desktop\\120_FUJI";
+
+            var filePaths = Directory.GetFiles(rootFolderPath);
+
+            //Dictionary<string, DateTime> dicFileDate = new Dictionary<string, DateTime>();
+
+            foreach (var filePath in filePaths)
+            {
+                ShellObject obj = ShellObject.FromParsingName(filePath);
+                var takenDate = obj.Properties.System.ItemDate.Value;
+                if (takenDate.HasValue)
+                {
+                    string folderPath = rootFolderPath + "\\" + takenDate.Value.ToString("yyyy-MM-dd");
+                    if (!Directory.Exists(folderPath))
+                        Directory.CreateDirectory(folderPath);
+
+                    FileInfo file = new FileInfo(filePath);
+                    file.MoveTo(folderPath + "\\" + file.Name);
+                }
+            }
+
+            Console.WriteLine("OK");
+            Console.ReadLine();
+        }
+
+        private static string GetFolderPath()
+        {
+            Console.Write("输入文件夹路径：");
+            var folderPath = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(folderPath))
+                GetFolderPath();
+
+            return folderPath;
+        }
+    }
+}
